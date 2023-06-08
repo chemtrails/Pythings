d4b54c3f8931885f23d40e98e193bb2fee3c87f5
chemtrails <rafalprazmo@protonmail.com>
2023-06-08 14:53:27 +0200

    Accept slices

--- looped_list.py
+++ looped_list.py
@@ -3,11 +3,17 @@ class LoopedList(list):
         super().__init__(iterable)
 
     def __setitem__(self, index, item):
+        super().__setitem__(self._convert(index), item)
+
+    def _convert(self, i):
         length = super().__len__()
-        index = index % length if index >= length else index
-        super().__setitem__(index, item)
+        return i % length if i >= length else i
 
     def __getitem__(self, index):
         length = super().__len__()
-        index = index % length if index >= length else index
-        return super().__getitem__(index)
+        if isinstance(index, slice):
+            return [
+                self.__getitem__(i)
+                for i in range(index.start or 0, index.stop or length, index.step or 1)
+            ]
+        return super().__getitem__(self._convert(index))
